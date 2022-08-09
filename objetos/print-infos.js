9279451934a30e0055b563a57662d689f059d147
lexart51 <luiz.cavolina.cunha@gmail.com>
2022-08-09 19:25:41 -0300

    curso de alura sobre java script

---
+++ objetos/print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345652266",
+    email:"andre@email.com"
+}
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`)
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos.`)
+console.log(cliente.cpf.substring(0,3))
